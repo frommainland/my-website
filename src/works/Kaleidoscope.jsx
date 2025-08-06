0dc7f05f8f2e06f276a780b70703c6140c00c22c
shuo <caihehuang@gmail.com>
2025-08-06 10:54:06 +0800

    update video demo

--- src/works/Kaleidoscope.jsx
+++ src/works/Kaleidoscope.jsx
@@ -11,11 +11,11 @@ const Kaleidoscope = () => {
 					<h1>Kaleidoscope</h1>
 					<p>
 						A simple tool to create kaleidoscope wallpaper with your
-						own choices and see it in motion with a different colored
-						lens
+						own choices and see it in motion with a different
+						colored lens
 					</p>
 				</header>
-
+				·
 				<section className="work-content-wrap">
 					<div className="work-content-text">
 						<p id="subtitle">Project Link</p>
@@ -36,6 +36,14 @@ const Kaleidoscope = () => {
 								/>
 							)
 						})}
+						<video
+							src={require('../video/kaleidoscope.mp4')}
+							autoPlay
+							muted
+							loop
+							playsInline
+							width="100%"
+						/>
 					</div>
 				</section>
 			</article>
